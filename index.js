b84eb9ff084398ca372f637796400abbc2f98895
mani <programmingfoo@gmail.com>
2020-05-30 10:05:23 -0700

    rest apis addeee

--- index.js
+++ index.js
@@ -4,22 +4,31 @@ const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const app = express()
 const port = 3000
-
+//routes
+const CourseRoutes = require('./routes/course')
+const DocumentRoutes = require('./routes/document')
+const ExerciseRoutes = require('./routes/exercise')
+const SeasonRoutes = require('./routes/season')
 //MIDDLE WARES
 app.use(cors());  //cors policy
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 ///
 
-mongoose.connect("mongodb://127.0.0.1:27017/?compressors=disabled&gssapiServiceName=mongodb", {
+mongoose.connect("mongodb+srv://root:pass@cluster0-i0azz.gcp.mongodb.net/test?retryWrites=true&w=majority/myNewDB", {
     useNewUrlParser: true,
     useUnifiedTopology: true
-}, () => console.log('  yeah you win !'));
+}, (err) => console.log('  yeah you win ! ' + (err ? err : '')));
 
 
 app.get('/', function (request, response) {
     response.send('hello')
 })
+//  our routes
+app.use('/courses', CourseRoutes)
+app.use('/documents', DocumentRoutes)
+app.use('/exercises', ExerciseRoutes)
+app.use('/seasons', SeasonRoutes)
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
 
